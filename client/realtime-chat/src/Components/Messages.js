6eb4b54fce3710456b1c3478bde03abee6577a5e
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-09-12 00:40:03 -0500

    Added missing classname for styling, basic core app is done, need a few tweaks

--- client/realtime-chat/src/Components/Messages.js
+++ client/realtime-chat/src/Components/Messages.js
@@ -5,7 +5,7 @@ import Message from './Message'
 import '../Styles/Messages.css'
 
 const Messages = ({ messages, name}) => (
-    <ScrollToBottom>
+    <ScrollToBottom className="messages">
         {messages.map((message, i) => <div key={i}><Message message={message} name={name}/></div>)}
     </ScrollToBottom>
 )
--- server/index.js
+++ server/index.js
@@ -18,10 +18,10 @@ io.on('connection', (socket) => {
 
         if(error) return callback(error);
 
-        socket.join(user.room)
-
-        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room '${user.room}'`});
         socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name}, has joined the room.`})
+
+        socket.join(user.room)
 
         io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)})
 
@@ -32,6 +32,7 @@ io.on('connection', (socket) => {
         const user = getUser(socket.id);
 
         io.to(user.room).emit('message', {user: user.name, text: message});
+        io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)});
 
         callback();
     })
