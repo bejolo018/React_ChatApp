1992bf5ac9a9ebab481834ab7e1b0483e543586d
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-09-11 19:57:06 -0500

    laid out some of chat.js, getting data when console logging server!

--- client/realtime-chat/src/Components/Chat.js
+++ client/realtime-chat/src/Components/Chat.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import queryString from 'query-string'
+import io from 'socket.io-client'
+
 import '../Styles/Chat.css'
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('')
+    const [room, setRoom] = useState('')
+    const ENDPOINT = 'localhost:5000'
+
+    useEffect(() => {
+        // location simply gives us the URL
+        const {name, room} = queryString.parse(location.search)
+
+        socket = io(ENDPOINT)
+
+        setName(name)
+        setRoom(room)
+
+
+        socket.emit('join', {name, room}, () =>{
+            
+        })
+        // Will only run hook IF these change
+    }, [ENDPOINT, location.search])
+
     return (
         <div>
             Chat
