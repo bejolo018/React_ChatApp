f36bdc6da6f41bc5cdf88895b9dd740f492b8d32
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-09-14 14:00:47 -0500

    created and styled header component

--- client/realtime-chat/src/App.js
+++ client/realtime-chat/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import {BrowserRouter as Router, Route } from 'react-router-dom'
 
+import Header from './Components/Header'
 import Join from './Components/Join'
 import Chat from './Components/Chat'
 
 const App = () => (
+    
     <Router>
+        <Header />
         <Route path='/' exact component={Join} />
         <Route path='/chat' component={Chat} />
     </Router>
---
+++ client/realtime-chat/src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import '../Styles/Header.css'
+
+const Header = () => (
+    <div>
+        <h1 className="header">
+            The Underground
+        </h1>
+        <a className="plug" href="https://benjaminjohnlopez.com">benjaminjohnlopez.com</a>
+    </div>
+
+)
+
+export default Header
